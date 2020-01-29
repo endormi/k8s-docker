9cf1b0c567addd3fdf53a50e179cec9870e61014
endormi <ernosalo14@gmail.com>
2020-01-30 01:22:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# k8s-docker
+
+Kubernetes with Docker
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/endormi/k8s-docker"
+          target="_blank"
+        >
+          GitHub Project
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mess',
+  data() {
+    return {
+      message: 'k8s-docker',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
