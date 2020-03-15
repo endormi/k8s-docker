ab8092b56ab9d5741de9fb8769be664ba4d4bf19
endormi <39559256+endormi@users.noreply.github.com>
2020-03-15 16:51:07 +0200

    Small updates

--- mkube.sh
+++ mkube.sh
@@ -3,6 +3,6 @@
 # First-time installation for Windows 10 Pro.
 # minikube start --vm-driver=hyperv --hyperv-virtual-switch=Minikube
 
-minikube start &&
-minikube status &&
+minikube start
+minikube status
 minikube dashboard
